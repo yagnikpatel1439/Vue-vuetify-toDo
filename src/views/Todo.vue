0b19ed64aba95bacbb585f182812764742c54792
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-06-10 18:05:33 -0400

    Vuetify adds beauty!

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,19 +2,31 @@
   <div class="home">
     <v-list
       flat
+      class="pt-0"
     >
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
-          </v-list-item-action>
+      <div
+      v-for="task in tasks"
+      :key="task.id"
+      >
+        <v-list-item
+        @click="doneTask(task.id)"
+        :class="{'blue lighten-5' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Wake Up sid!</v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
-
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+      
     </v-list>
   </div>  
 </template>
@@ -29,17 +41,26 @@
         tasks: [
           {
             id: 1,
-            title: 'Wake up'
+            title: 'Wake up',
+            done: false 
           },
            {
             id: 2,
-            title: 'Bananas'
+            title: 'Bananas',
+            done: false 
           },
           {
             id: 3,
-            title: 'Ape it'
+            title: 'Ape it',
+            done: false           
           },
         ]
+      }
+    },
+    methods: {
+      doneTask(id) {
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done
       }
     }
   }
